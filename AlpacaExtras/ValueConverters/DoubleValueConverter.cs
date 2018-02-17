b175427bb4ba302521a2821f27b544e9e1bf8726
CLYDE-DESKTOP\Clyde <clydeu@codev.com>
2018-02-17 16:44:10 +0800

    add BorderRadius property and bugfix text binding for button controls.

---
+++ AlpacaExtras/ValueConverters/DoubleValueConverter.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AlpacaExtras.ValueConverters
+{
+    public class DoubleValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value ?? string.Empty).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double.TryParse(value.ToString(), out double i);
+            return i;
+        }
+    }
+}
